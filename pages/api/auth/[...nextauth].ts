1f349921c4bb58194ae3de7b3b87cf1bb0f76e55
TienTein <tranxuantien.tein@gmail.com>
2023-05-05 20:53:37 +0700

    delete callback to build

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,22 +10,6 @@ export default NextAuth({
     }),
   ],
   secret: "94d25574656a8ee61f1adb9d18435c36",
-  callbacks: {
-    async session({ session, token, user }) {
-      session.user.id = token.id;
-      session.accessToken = token.accessToken;
-      return session;
-    },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      if (user) {
-        token.id = user.id;
-      }
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-  },
 });
 
 //clientId:586486200042-n2gsukec90iep47p3erp515l30m5ar68.apps.googleusercontent.com
